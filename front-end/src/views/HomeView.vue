71cc6e3e95ba4edf3052d3dff4c009e8cb91e330
MasihTak <masihtak@outlook.com>
2022-03-01 18:31:07 +0100

    chore: add some basic elements to the home view

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <p>This is the Home view</p>
+  <div class="container">
+    <h2> 👋 Hi there! Let's get started on your heads up!</h2>
+    <div class="card text-center">
+      <p>Hey there</p>
+    </div>
+  </div>
 </template>
 
 <script>
