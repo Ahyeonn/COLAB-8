a4e933ea1c7da161ca4faabed732ace41ecd619c
MasihTak <masihtak@outlook.com>
2022-03-01 18:30:26 +0100

    feat: add header component

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,8 +1,16 @@
+<script setup>
   import TheFooter from "@/components/TheFooter";
+  import TheHeader from "@/components/TheHeader";
+</script>
+
 <template>
+  <TheHeader />
   <router-view/>
   <TheFooter />
 </template>
 
 <style lang="scss">
+  #app {
+      height: 100%;
+    }
 </style>
---
+++ front-end/src/components/TheHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">Navbar</a>
+      <div class="break flex-grow-1"></div>
+      <img class="img-fluid rounded-circle" src="@/assets/user.jpg" alt="User profile image">
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+}
+</script>
+
+<style scoped lang="scss">
+  nav {
+    color: $white;
+  }
+  img {
+    aspect-ratio: 1/1;
+    width: 42px;
+    height: 42px;
+    }
+</style>
