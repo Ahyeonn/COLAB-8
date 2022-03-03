cf1984a5bd4b514ab650dbe3d2f4118f7fdd8eed
MasihTak <masihtak@outlook.com>
2022-03-03 20:49:21 +0100

    feat: make the navbar functional

--- front-end/src/components/TheHeader.vue
+++ front-end/src/components/TheHeader.vue
@@ -1,22 +1,34 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" :class="{ 'round-nav': isVisible }">
     <div class="container-fluid">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          @click="toggleNavbar"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <a class="navbar-brand" href="#">Navbar</a>
       <div class="break flex-grow-1"></div>
       <img class="img-fluid rounded-circle" src="@/assets/user.jpg" alt="User profile image">
-      <div class="collapse navbar-collapse" id="navbarToggler">
+      <div class="collapse navbar-collapse" :class="{ show: isVisible }" id="navbarToggler">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <router-link class="nav-link" to="/">Home</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <router-link class="nav-link" to="/how-it-works">How it works</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <router-link class="nav-link" to="/log-in">Log in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/sign-in">Sign in</router-link>
           </li>
         </ul>
       </div>
@@ -26,17 +38,46 @@
 
 <script>
 export default {
-  name: "TheHeader"
+  name: "TheHeader",
+  data() {
+    return {
+      isVisible: false
+    }
+  },
+  methods: {
+    toggleNavbar() {
+      this.isVisible = !this.isVisible;
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
-  nav {
-    color: $white;
+@import "src/scss/utilities/variables";
+
+nav {
+  color : $white;
   }
-  img {
-    aspect-ratio: 1/1;
-    width: 42px;
-    height: 42px;
-    }
+
+.round-nav, .show {
+  border-bottom-left-radius  : 30px;
+  border-bottom-right-radius : 30px;
+  }
+
+.show {
+  background : white;
+  margin     : 5px -12px -10px !important;
+  padding    : 0 20px;
+  box-shadow : 0 3px 10px rgba(0, 0, 0, .3);
+  }
+
+.nav-item:not(:last-child) {
+  border-bottom : 1px solid $color-secondary;
+  }
+
+img {
+  aspect-ratio : 1/1;
+  width        : 42px;
+  height       : 42px;
+  }
 </style>
