ddd76b9c996781c1ad9ec88a5663ed8cc21e4105
MasihTak <masihtak@outlook.com>
2022-03-03 20:48:33 +0100

    feat: add how it works view

---
+++ front-end/src/views/HowItWorks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h2> 👀 How does it work?</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HowitWorks"
+}
+</script>
+
+<style scoped>
+
+</style>
