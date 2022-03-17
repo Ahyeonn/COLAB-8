f3b4c4a0b502c0221bf8dbaf88b564fbcc462902
Ahyeonn <ahyeon971030@gmail.com>
2022-03-17 01:02:35 -0700

    add: validate_number function

--- back-end/database.py
+++ back-end/database.py
@@ -1,9 +1,17 @@
 from pymongo import MongoClient
 import os
+import re
 
 uri = os.environ.get('MONGODB_URI')
 client = MongoClient(uri)
 db = client.Colab8
+
 users = db.users
 users.create_index("phone_number", unique = True)
 events = db.events
+
+def validate_number(number):
+    if re.search(r'^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$', number):
+        return True
+    else:
+        return False
