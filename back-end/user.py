f15cea597fcf374518fc0ee4548443c4785d959b
Ahyeonn <ahyeon971030@gmail.com>
2022-03-23 09:55:40 -0700

    fix: error_handling for signup route

--- back-end/user.py
+++ back-end/user.py
@@ -31,12 +31,10 @@ def signup():
     password = request.json['password']
 
     if users.find_one({'phone_number': phone_number}):
-        return ValueError('The phone number already exists!')
-        # abort(403, description='The phone number already exists!')
+        abort(403, description='The phone number already exists!')
     if validate_number(phone_number): # +1 123-456-7890
         if len(password) < 8:
-            raise ValueError('Password needs to be minimum 8 characters')
-            # abort(403, description='Password needs to be minimum 8 characters')
+            abort(403, description='Password needs to be minimum 8 characters')
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         user_id = users.insert_one({
             '_id': uuid.uuid4().hex,
@@ -49,8 +47,7 @@ def signup():
         session['current_user'] = new_user
         return jsonify([{'name' : new_user['name'], 'phone_number': new_user['phone_number']}]), 201
     else:
-        raise ValueError('Type the correct number')
-        # abort(403, description='Type the correct number')
+        abort(403, description='Type the correct number')
 
 @user.route('/signin', methods=['POST'])
 def signin():
